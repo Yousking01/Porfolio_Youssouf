5d0cd4708c459bc1b8efec071c9dd69cfd0f9393
Yousking01 <djireyoussouf1999@gmail.com>
2023-04-29 17:31:52 +0200

    29/04/2023/1

--- forms/contact.php
+++ forms/contact.php
@@ -2,15 +2,15 @@
 //get data from form  
 $name = $_POST['name'];
 $email= $_POST['email'];
-$subject= $_POST['subject'];
+// $subject= $_POST['subject'];
 $message= $_POST['message'];
 $to = "djireyoussouf1999@gmail.com";
 $subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Subject =" . $subject . "\r\n Message =" . $message;
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
-header("Location:thankyou.html");
+// header("Location:thankyou.html");
 ?>
