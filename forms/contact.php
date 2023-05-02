1a2b75a62e08db552a2fcacb16aaa0c940be4a7b
Yousking01 <djireyoussouf1999@gmail.com>
2023-05-02 18:13:05 +0200

    02/05/2023/3

--- forms/contact.php
+++ forms/contact.php
@@ -1,16 +1,43 @@
 <?php
-      //Si le formulaire a étét soumis
-      if (isset($_POST["message"])) {
-        $message = "Ce message vous a été envoyé via la page contact de votre portfolio
-        Name : " . $_POST["name"] . "
-        Email : " . $_POST["email"] . "
-        Subject : " . $_POST["subject"] . "
-        Message : " . $_POST["message"];
+use PHPMailer\PHPMailer\PHPMailer;
 
-        $retour = mail("djireyoussouf1999@gmail.com", $_POST["subject"],
-        $message, "Reply-to:" . $_POST["email"]);
-        if ($retour) {
-          echo "<p>L'email a bien été envoyé.</p>";
-        }
-      }
-    ?>
+
+require_once "PHPMailer/PHPMailer.php";
+    require_once "PHPMailer/SMTP.php";
+    require_once "PHPMailer/Exception.php";
+
+    $mail = new PHPMailer();
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    try{
+      //smtp settings
+    $mail->isSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->Username = "djireyoussouf1999@gmail.com";
+    $mail->Password = 'Tenoukan1999';
+    
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = '587';
+
+    $mail->setFrom("djireyoussouf1999@gmail.com");
+    $mail->addAddress("djireyoussouf1999@gmail.com");
+
+    $mail->isHTML(true);
+    $mail->Subject = 'Messge réçu a partir de la page contact de votre portfolio';
+    $mail->Body = '<h3>Name: $name <br>Email: $email <br>Message: $message</h3>';
+    
+    $mail->send();
+      
+  } catch (Exception $e){
+    
+  }
+}
+
+ 
+?>
